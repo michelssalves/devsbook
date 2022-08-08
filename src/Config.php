db3bf526a25457dd816f9cc814586b13392b8f18
unknown <michelssalves@gmail.com>
2022-08-08 20:52:57 -0300

    2022-08-08

--- src/Config.php
+++ src/Config.php
@@ -6,9 +6,9 @@ class Config {
 
     const DB_DRIVER = 'mysql';
     const DB_HOST = 'localhost';
-    const DB_DATABASE = 'devsbook';
-    CONST DB_USER = 'root';
-    const DB_PASS = '';
+    const DB_DATABASE = '338658684_ambiente_de_te';
+    CONST DB_USER = '338658684_ambiente_de_te';
+    const DB_PASS = 'Cavalo*123';
 
     const ERROR_CONTROLLER = 'ErrorController';
     const DEFAULT_ACTION = 'index';
